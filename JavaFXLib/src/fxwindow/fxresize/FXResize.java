628029a07f4419a435b865b176bbfb15cb3f177d
Ivan Zhen <ivangpajon@hotmail.com>
2017-11-15 23:30:38 +0100

    vertical and horizontal resizing implemented

--- JavaFXLib/src/fxwindow/fxresize/FXResize.java
+++ JavaFXLib/src/fxwindow/fxresize/FXResize.java
@@ -9,8 +9,7 @@ public class FXResize {
 	
 	private AnchorPane root;
 	private Pane paneUp, paneRight, paneDown, paneLeft;
-	private double initY, initHeight;
-	
+	private double initY, initX, initWidth, initHeight;
 	
 	public FXResize() {
 		
@@ -49,6 +48,23 @@ public class FXResize {
 		this.paneLeft = paneLeft;
 	}
 	
+	public void setVerticalPanes(Pane paneUp, Pane paneDown) {
+		this.paneUp = paneUp;
+		this.paneDown = paneDown;
+	}
+	
+	public void setHorizontalPanes(Pane paneLeft, Pane paneRight) {
+		this.paneLeft = paneLeft;
+		this.paneRight = paneRight;
+	}
+	
+	public void setAllPanes(Pane paneUp, Pane paneRight, Pane paneDown, Pane paneLeft) {
+		this.paneUp = paneUp;
+		this.paneRight = paneRight;
+		this.paneDown = paneDown;
+		this.paneLeft = paneLeft;
+	}
+	
 	public void resizeVerticalPressed(MouseEvent e) {
         Stage stage = (Stage) root.getScene().getWindow();
         
@@ -61,5 +77,31 @@ public class FXResize {
         
         stage.setHeight(initHeight-(e.getScreenY()-initY));
         stage.setY(e.getScreenY());
+    }
+    
+    public void resizeDownDragged(MouseEvent e) {
+        Stage stage = (Stage) root.getScene().getWindow();
+        
+        stage.setHeight(e.getScreenY()-initY);
+    }
+    
+    public void resizeHorizontalPressed(MouseEvent e) {
+    	Stage stage = (Stage) root.getScene().getWindow();
+        
+        initWidth = stage.getWidth();
+        initX = stage.getX();
+    }
+    
+    public void resizeLeftDragged(MouseEvent e) {
+    	Stage stage = (Stage) root.getScene().getWindow();
+        
+    	stage.setWidth(initWidth+(initX-e.getScreenX()));
+        stage.setX(e.getScreenX());
+    }
+    
+    public void resizeRightDragged(MouseEvent e) {
+    	Stage stage = (Stage) root.getScene().getWindow();
+        
+        stage.setWidth(e.getScreenX()-initX);
     }
 }
