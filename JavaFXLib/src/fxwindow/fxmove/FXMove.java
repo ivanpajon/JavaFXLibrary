2fd6806f50c9694c7f2451bfa612f62c56f1c86c
Ivan Zhen <ivangpajon@hotmail.com>
2017-11-16 00:17:06 +0100

    updated FXMove

--- JavaFXLib/src/fxwindow/fxmove/FXMove.java
+++ JavaFXLib/src/fxwindow/fxmove/FXMove.java
@@ -23,6 +23,7 @@ public class FXMove {
 	
 	public void movePressed(MouseEvent e) {
 		Stage stage = (Stage) root.getScene().getWindow();
+		
 		if (!stage.isMaximized()) {
 		    initX = e.getSceneX();
 		    initY = e.getSceneY();
@@ -31,6 +32,7 @@ public class FXMove {
 	
 	public void moveDragged(MouseEvent e) {
 		Stage stage = (Stage) root.getScene().getWindow();
+		
 		if (!stage.isMaximized()) {
 	        stage.setX(e.getScreenX()-initX);
 	        stage.setY(e.getScreenY()-initY);
@@ -41,4 +43,29 @@ public class FXMove {
 	        stage.setY(e.getScreenY()-initY);
 		}
 	}
+	
+	public void dragToUp(MouseEvent e) {
+		Stage stage = (Stage) root.getScene().getWindow();
+ 		
+ 		if (e.getScreenY() < 10) {
+ 			stage.setMaximized(true);
+ 		}
+ 	}
+	
+	public void dragToCenter(MouseEvent e) {
+		Stage stage = (Stage) root.getScene().getWindow();
+ 		
+ 		if (e.getScreenY() >= 10) {
+ 			stage.setMaximized(false);
+ 		}
+ 	}
+	
+	public boolean isDraggedToUp(MouseEvent e) {
+ 		if (e.getScreenY() < 10) {
+ 			return true;
+ 		}
+ 		else {
+ 			return false;
+ 		}
+ 	}
 }
