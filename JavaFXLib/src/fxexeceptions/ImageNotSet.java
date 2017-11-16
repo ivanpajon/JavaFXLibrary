b80e785ba2629b4a19415872d3a123fe92667053
Ivan Zhen <ivangpajon@hotmail.com>
2017-11-16 20:02:15 +0100

    package fxexceptions for custom exceptions added

---
+++ JavaFXLib/src/fxexeceptions/ImageNotSet.java
@@ -0,0 +1,10 @@
+package fxexeceptions;
+
+public class ImageNotSet extends Exception {
+	private static final long serialVersionUID = 919409365054623198L;
+	
+	public ImageNotSet() {
+		super("Image can't be null");
+	}
+	
+}
