137026e1292c7f15576ea4507b2a4dc32fab2e2d
Ivan Zhen <ivangpajon@hotmail.com>
2017-11-18 19:30:48 +0100

    Javadoc implemented

--- JavaFXLib/src/fxexeceptions/ImageNotSet.java
+++ JavaFXLib/src/fxexeceptions/ImageNotSet.java
@@ -1,12 +1,24 @@
 package fxexeceptions;
 
+/**
+ * This class captures errors if not set any image
+ * @author Ivan Zhen
+ * @version 1.0
+ */
 public class ImageNotSet extends Exception {
 	private static final long serialVersionUID = 919409365054623198L;
 	
+	/**
+	 * Create a new ImageNotSet error with default message
+	 */
 	public ImageNotSet() {
 		super("Image can't be null");
 	}
 	
+	/**
+	 * Create a new ImageNotSet error with custom message
+	 * @param message Message to show in error description
+	 */
 	public ImageNotSet(String message) {
 		super(message);
 	}
