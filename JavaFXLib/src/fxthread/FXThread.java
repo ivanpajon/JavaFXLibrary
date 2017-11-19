e1c1bb2f390dd1f551c6895ace146f1320eceea3
Ivan Zhen <ivangpajon@hotmail.com>
2017-11-19 18:31:46 +0100

    Implemented JavaDoc

--- JavaFXLib/src/fxthread/FXThread.java
+++ JavaFXLib/src/fxthread/FXThread.java
@@ -1,26 +1,47 @@
 package fxthread;
 
+/**
+ * This class allows to create a Thread and manage it
+ * @author Ivan Zhen
+ * @version 1.0
+ */
 public class FXThread {
 	private Thread t;
 	private Runnable r;
 	
+	/**
+	 * Creates new empty FXThread
+	 */
 	public FXThread() {
 		
 	}
 	
+	/**
+	 * Sets task for run when Thread starts
+	 * @param r Runnable for assign to the thread
+	 */
 	public void setTask(Runnable r) {
 		this.r = r;
 	}
 	
+	/**
+	 * Creates new Thread with its Runnable
+	 */
 	public void createThread() {
 		this.t = new Thread(this.r);
 		this.t.setDaemon(true);
 	}
 	
+	/**
+	 * Starts the Thread
+	 */
 	public void startThread() {
 		this.t.start();
 	}
 	
+	/**
+	 * Stops the Thread
+	 */
 	public void stopThread() {
 		this.t.interrupt();
 	}
