fe913e8715ca45e96aabc49ade1936fa388c444b
Ivan Zhen <ivangpajon@hotmail.com>
2017-11-21 19:36:29 +0100

    Updated FXStage and implement real-time changing Scene

--- JavaFXLib/src/fxwindow/fxstage/FXStage.java
+++ JavaFXLib/src/fxwindow/fxstage/FXStage.java
@@ -3,7 +3,9 @@ package fxwindow.fxstage;
 import java.io.IOException;
 import java.nio.file.Paths;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
@@ -35,6 +37,10 @@ public class FXStage {
 		this.undecorated = false;
 	}
 	
+	public void setContext(AnchorPane root) {
+		this.root = root;
+	}
+	
 	public void setUrl(String url) {
 		this.url = url;
 	}
@@ -43,8 +49,26 @@ public class FXStage {
 		this.title = title;
 	}
 	
+	public void setScene(String url) {
+		try {
+			this.root = FXMLLoader.load(Paths.get(url).toUri().toURL());
+	        this.scene = new Scene(this.root);
+		}
+		catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+	
 	public void setUndecorated(Boolean b) {
 		this.undecorated = b;
+	}
+	
+	public void updateScene(ActionEvent e) {
+		this.stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+		
+		this.stage.setScene(this.scene);
+		this.stage.setTitle(this.title);
+		this.stage.show();
 	}
 	
 	public void open() {
