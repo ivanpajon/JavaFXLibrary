ffe3352a227c7311150ebb415e7dc65b64257945
IvanZhen <ivan@codemain.es>
2017-11-21 10:31:57 +0100

    Updated and implemented FXStage

--- JavaFXLib/src/fxwindow/fxstage/FXStage.java
+++ JavaFXLib/src/fxwindow/fxstage/FXStage.java
@@ -1,38 +1,72 @@
 package fxwindow.fxstage;
 
 import java.io.IOException;
-import java.net.URL;
+import java.nio.file.Paths;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class FXStage {
-	private URL url;
+	private String url;
 	private String title;
+	private boolean undecorated;
+	private boolean open;
+	private AnchorPane root;
+	private Scene scene;
+	private Stage stage;
 	
 	public FXStage() {
-		
+		this.stage = new Stage();
+		this.undecorated = false;
+		this.open = false;
 	}
 	
-	public FXStage(URL url, String title) {
+	public FXStage(String url, String title) {
 		this.url = url;
 		this.title = title;
+		this.stage = new Stage();
+		this.undecorated = false;
+		this.open = false;
 	}
 	
 	public void open() {
 		try {
-			Stage secondaryStage = new Stage();
-			AnchorPane root = FXMLLoader.load(getClass().getResource(this.url.getFile().replaceAll("%20", " ")));
-	        Scene scene = new Scene(root);
-	        secondaryStage.setScene(scene);
-	        secondaryStage.setTitle(this.title);
-	        
-	        secondaryStage.show();
+			if (!open) {
+				if (undecorated) {
+					this.root = FXMLLoader.load(Paths.get(this.url).toUri().toURL());
+			        this.scene = new Scene(this.root);
+			        this.stage.setScene(this.scene);
+			        this.stage.setTitle(this.title);
+			        this.stage.initStyle(StageStyle.TRANSPARENT);
+					this.scene.setFill(Color.TRANSPARENT);
+			        
+			        this.stage.show();
+			        this.open = true;
+				}
+				else {
+					this.root = FXMLLoader.load(Paths.get(this.url).toUri().toURL());
+			        this.scene = new Scene(this.root);
+			        this.stage.setScene(this.scene);
+			        this.stage.setTitle(this.title);
+			        
+			        this.stage.show();
+			        this.open = true;
+				}
+			}
+			else {
+				this.stage.show();
+			}
 		}
 		catch (IOException e) {
 			System.out.println(e);
 		}
+	}
+	
+	public void setUndecorated(Boolean b) {
+		this.undecorated = b;
 	}
 }
