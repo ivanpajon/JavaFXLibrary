d2a22c27f48a5aac1d31948059dff2637a882877
IvanZhen <ivan@codemain.es>
2017-11-21 12:12:14 +0100

    Updated FXStage

--- JavaFXLib/src/fxwindow/fxstage/FXStage.java
+++ JavaFXLib/src/fxwindow/fxstage/FXStage.java
@@ -14,32 +14,25 @@ public class FXStage {
 	private String url;
 	private String title;
 	private boolean undecorated;
-	private boolean open;
 	private AnchorPane root;
 	private Scene scene;
 	private Stage stage;
 	
 	public FXStage() {
-		this.stage = new Stage();
 		this.undecorated = false;
-		this.open = false;
 	}
 	
 	public FXStage(String url) {
 		this.url = url;
 		
-		this.stage = new Stage();
 		this.undecorated = false;
-		this.open = false;
 	}
 	
 	public FXStage(String url, String title) {
 		this.url = url;
 		this.title = title;
 		
-		this.stage = new Stage();
 		this.undecorated = false;
-		this.open = false;
 	}
 	
 	public void setUrl(String url) {
@@ -56,30 +49,25 @@ public class FXStage {
 	
 	public void open() {
 		try {
-			if (!open) {
-				if (undecorated) {
-					this.root = FXMLLoader.load(Paths.get(this.url).toUri().toURL());
-			        this.scene = new Scene(this.root);
-			        this.stage.setScene(this.scene);
-			        this.stage.setTitle(this.title);
-			        this.stage.initStyle(StageStyle.TRANSPARENT);
-					this.scene.setFill(Color.TRANSPARENT);
-			        
-			        this.stage.show();
-			        this.open = true;
-				}
-				else {
-					this.root = FXMLLoader.load(Paths.get(this.url).toUri().toURL());
-			        this.scene = new Scene(this.root);
-			        this.stage.setScene(this.scene);
-			        this.stage.setTitle(this.title);
-			        
-			        this.stage.show();
-			        this.open = true;
-				}
+			if (undecorated) {
+				this.stage = new Stage();
+				this.root = FXMLLoader.load(Paths.get(this.url).toUri().toURL());
+		        this.scene = new Scene(this.root);
+		        this.stage.setScene(this.scene);
+		        this.stage.setTitle(this.title);
+		        this.stage.initStyle(StageStyle.TRANSPARENT);
+				this.scene.setFill(Color.TRANSPARENT);
+		        
+		        this.stage.show();
 			}
 			else {
-				this.stage.show();
+				this.stage = new Stage();
+				this.root = FXMLLoader.load(Paths.get(this.url).toUri().toURL());
+		        this.scene = new Scene(this.root);
+		        this.stage.setScene(this.scene);
+		        this.stage.setTitle(this.title);
+		        
+		        this.stage.show();
 			}
 		}
 		catch (IOException e) {
