98a8d0b3b7e911d393f6ca813021eed8e826ed28
Ivan Zhen <ivangpajon@hotmail.com>
2017-11-25 17:05:06 +0100

    Added JavaDoc

--- JavaFXLib/src/fxexeceptions/SceneNotSet.java
+++ JavaFXLib/src/fxexeceptions/SceneNotSet.java
@@ -1,12 +1,24 @@
 package fxexeceptions;
 
+/**
+ * <h1>This class captures errors if not set any scene</h1>
+ * @author Ivan Zhen
+ * @version 1.0
+ */
 public class SceneNotSet extends Exception {
 	private static final long serialVersionUID = 8316916157705665051L;
 	
+	/**
+	 * Creates a new SceneNotSet error with default message
+	 */
 	public SceneNotSet() {
 		super("Scene can't be null");
 	}
 	
+	/**
+	 * Creates a new SceneNotSet error with custom message
+	 * @param message Message to show in error description
+	 */
 	public SceneNotSet(String message) {
 		super(message);
 	}
