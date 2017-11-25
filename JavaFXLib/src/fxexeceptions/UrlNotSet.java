4c4a75e34b48652b5582cbcb04ffc2af409670f9
Ivan Zhen <ivangpajon@hotmail.com>
2017-11-25 17:03:50 +0100

    Added JavaDoc

--- JavaFXLib/src/fxexeceptions/UrlNotSet.java
+++ JavaFXLib/src/fxexeceptions/UrlNotSet.java
@@ -1,12 +1,24 @@
 package fxexeceptions;
 
+/**
+ * <h1>This class captures errors if not set any url</h1>
+ * @author Ivan Zhen
+ * @version 1.0
+ */
 public class UrlNotSet extends Exception {
 	private static final long serialVersionUID = -8133437171497735464L;
 	
+	/**
+	 * Creates a new UrlNotSet error with default message
+	 */
 	public UrlNotSet() {
 		super("Url can't be null");
 	}
 	
+	/**
+	 * Creates a new UrlNotSet error with custom message
+	 * @param message Message to show in error description
+	 */
 	public UrlNotSet(String message) {
 		super(message);
 	}
